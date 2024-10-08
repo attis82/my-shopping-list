d932ced5d8db511e1adbc3803d688199b3c66a3e
attis82 <farkasatesz.82@gmail.com>
2024-10-08 10:41:13 +0200

    Gets colors for the app

--- app/src/main/java/com/farkasatesz/myshoppinglist/ui/theme/Color.kt
+++ app/src/main/java/com/farkasatesz/myshoppinglist/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BgColor = Color(0xFF243642)
+val TextColor = Color(0xFFE2F1E7)
+val CardColor = Color(0xFF387478)
